c4a7e7d5eaafcbcd1b91841bde3240a1555b2809
Gabriela Santana <gabriela2santtanna@gmail.com>
2024-06-01 01:50:20 -0300

    SmallChanges

---
+++ ifsp-projeto-desempenho-de-algoritmos/desempenhoDeAlgoritmos/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "desempenhoDeAlgoritmos.h"
+
+int main()
+{
+    int escolhaAlgoritmo = 0;
+    do{
+        system("cls");
+        int escolhaElementos = 0;
+        escolhaAlgoritmo = 0;
+        escolhaMenuAlgoritmos();
+        scanf("%d", &escolhaAlgoritmo);
+        fflush(stdin);
+        if(!(escolhaAlgoritmo >= 1 && escolhaAlgoritmo <= 11)){
+            printf("\nERRO - Valor invalido. Por favor, insira um valido.\n\n");
+            system("pause");
+            continue;
+        }
+        if(escolhaAlgoritmo != 11){
+            while(!(escolhaElementos >= 1 && escolhaElementos <= 7)){
+                system("cls");
+                escolhaMenuElementos();
+                scanf("%d", &escolhaElementos);
+                if(escolhaElementos < 1 || escolhaElementos > 7){
+                    printf("\nERRO - Valor invalido. Por favor, insira um valido.\n\n");
+                    system("pause");
+                    fflush(stdin);
+                    continue;
+                }
+                if(escolhaElementos == 7){
+                    continue;
+                }
+                double tempoDeExecucao = medicaoDeTempo(escolhaAlgoritmo, escolhaElementos);
+                printf("\nTempo medio de ordenacao do %s: %.3f microssegundos\n\n", nomeAlgoritmo(escolhaAlgoritmo), tempoDeExecucao);
+                fflush(stdin);
+                system("pause");
+            }
+        }else{
+            printf("\nEncerrando...\n");
+        }
+    }while(escolhaAlgoritmo != 11);
+}
